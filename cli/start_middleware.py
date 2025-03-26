f6725b601b48f9fa318d8967e06a4d7a3e78ac8a
sandwich1699975 <freddymcloughlan@hotmail.com>
2025-03-27 09:22:36 +1100

    Added root search for binary

--- cli/start_middleware.py
+++ cli/start_middleware.py
@@ -2,12 +2,28 @@ import logging
 import cli.proccess as process
 import os
 import enum
+from typing import Optional
 
 
 class InterfaceType(enum.Enum):
     # Reference the main middleware cpp file
     UART = "UART"
     TEST = "TEST"
+
+
+def get_middleware_path(BINARY_NAME) -> Optional[str]:
+    """Check if middleware is in build/ then check if it is in root folder.
+    This helps when sharing releases, but still prioritises the build/ folder.
+    """
+    build_path = os.path.join(".", "build", BINARY_NAME),
+    root_path = os.path.join(".", BINARY_NAME),
+
+    if os.path.exists(build_path):
+        return build_path
+    elif os.path.exists(root_path):
+        return root_path
+    else:
+        return None
 
 
 def start_middleware(logger: logging.Logger,
@@ -22,12 +38,17 @@ def start_middleware(logger: logging.Logger,
             f"INTERFACE_TYPE must be a InterfaceType value, got: {INTERFACE_TYPE} as type {type(INTERFACE_TYPE)}")
     try:
 
-        BINARY = "middleware_server_release" if release else "middleware_server"
+        BINARY_NAME = "middleware_server_release" if release else "middleware_server"
+        MIDDLEWARE_BINARY_PATH = get_middleware_path(BINARY_NAME)
+
+        if MIDDLEWARE_BINARY_PATH is None:
+            raise FileNotFoundError(
+                f"Could not find middleware binary ({BINARY_NAME}) in build/ or root folder")
 
         MIDDLEWARE_COMMAND = [
             # Should always be relative to cwd. Just use the (.):
             # ./middleware/build/middleware_server {args}
-            os.path.join(".", "build", BINARY),
+            MIDDLEWARE_BINARY_PATH,
             # <interface type> <device path> <socket path>
             INTERFACE_TYPE.value,
             DEVICE_PATH,
