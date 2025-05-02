b61eb81d3c2330feac66cdccdbb414731227153e
Amber Taylor <167683926+s4105951@users.noreply.github.com>
2025-05-02 14:33:00 +1000

    API memory improvements

--- frontend/static/js/GCS_API.js
+++ frontend/static/js/GCS_API.js
@@ -21,9 +21,11 @@ function feetToMetres(feet) {
 }
 
 // API
+const initialReconnectInterval = 200;
+const maxReconnectInterval = 5000;
+
 var api_socket;
-var reconnectInterval = 1000;
-var maxReconnectInterval = 10000;
+var reconnectInterval = initialReconnectInterval;
 var reconnectTimeout;
 
 function scheduleReconnect() {
@@ -37,142 +39,140 @@ function scheduleReconnect() {
 }
 
 function API_socketConnect() {
-    api_url = window.location.host.split(":")[0];
+    const api_url = window.location.host.split(":")[0];
     api_socket = new WebSocket(`ws://${api_url}:1887`);
 
     api_socket.onopen = () => {
         console.log(`connection gaming - ${api_url}`);
         clearTimeout(reconnectTimeout);
-        reconnectInterval = 1000;
+        reconnectInterval = initialReconnectInterval;
     };
 
     api_socket.onmessage = API_OnMessage;
 
     api_socket.onerror = (error) => {
         console.error("websocket error: ", error);
-        api_socket.close();
     };
 
     api_socket.onclose = () => {
-        console.log("socket closed: attempting to reconnect");
+        console.log("Socket closed: attempting to reconnect automatically");
         scheduleReconnect();
     };
 }
 
 function API_OnMessage(event) {
+    let apiLatest, apiData;
     try {
         //console.log(JSON.parse(event.data));
-
-        const api_latest = JSON.parse(event.data);
-        const api_data = api_latest.data;
-        api_data.id = api_latest.id;
-
-        // Additional processing of API data for display
-        processDataForDisplay(api_data);
+        apiLatest = JSON.parse(event.data);
+        apiData = processDataForDisplay(apiLatest.data, apiLatest.id);
 
         // Send data to display
-        if (api_latest.id == 3 || api_latest.id == 4 || api_latest.id == 5) {
-            // AVIONICS PACKETS
-            api_data._radio = "av1";
-            api_data.meta.packets = ++packetsAV1;
+        // HANDLE AVIONICS PACKETS
+        if ([3, 4, 5].includes(apiLatest.id)) {
+            apiData._radio = "av1";
+            apiData.meta.packets = ++packetsAV1;
 
             /// AV DISPLAY VALUES
             // Radio module
             if (typeof displayUpdateRadio === "function") {
-                displayUpdateRadio(api_data);
+                displayUpdateRadio(apiData);
             }
 
             // Avionics module
             if (typeof displayUpdateAvionics === "function") {
-                displayUpdateAvionics(api_data);
+                displayUpdateAvionics(apiData);
             }
 
             // Position module
             if (typeof displayUpdatePosition === "function") {
-                displayUpdatePosition(api_data);
+                displayUpdatePosition(apiData);
             }
 
             // Flight state module
             if (typeof displayUpdateFlightState === "function") {
-                displayUpdateFlightState(api_data);
+                displayUpdateFlightState(apiData);
             }
 
             /// AV GRAPHS
             if (typeof graphUpdateAvionics === "function") {
-                graphUpdateAvionics(api_data);
+                graphUpdateAvionics(apiData);
             }
 
             if (typeof graphUpdatePosition === "function") {
-                graphUpdatePosition(api_data);
+                graphUpdatePosition(apiData);
             }
 
             /// AV ROCKET
             // Rocket module
             if (typeof rocketUpdate === "function") {
-                rocketUpdate(api_data);
+                rocketUpdate(apiData);
             }
-            
-        } else if (api_latest.id == 6 || api_latest.id == 7) {
-            // GSE PACKETS
-            api_data._radio = "gse";
-            api_data.meta.packets = ++packetsGSE;
+        }
+        // HANDLE GSE PACKETS
+        else if ([6, 7].includes(apiLatest.id)) {
+            apiData._radio = "gse";
+            apiData.meta.packets = ++packetsGSE;
 
             /// GSE DISPLAY VALUES
             // Radio module
             if (typeof displayUpdateRadio === 'function') {
-                displayUpdateRadio(api_data);
+                displayUpdateRadio(apiData);
             }
 
             // Auxilliary data module
             if (typeof displayUpdateAuxData === 'function') {
-                displayUpdateAuxData(api_data);
+                displayUpdateAuxData(apiData);
             }
 
             /// GSE GRAPHS
             if (typeof graphUpdateAuxData === "function") {
-                graphUpdateAuxData(api_data);
+                graphUpdateAuxData(apiData);
             }
             /*
-            displayUpdatePayload(api_data);
+            displayUpdatePayload(apiData);
             */
         }
     } catch (error) {
-        console.error("data error");
-        console.error(error);
+        console.error("Data processing error:", error);
     }
 }
 
-function processDataForDisplay(api_data) {
+function processDataForDisplay(apiData, apiId) {
     // Process data from the API for display
+    const processedData = { ...apiData }; // Shallow copy
+    processedData.id = apiId;
 
     // Acceleration
     // Determine whether to use low or high precision values
-    if (api_data.accelLowX != undefined && api_data.accelHighX != undefined) {
-        api_data.accelX =
-            Math.abs(api_data.accelHighX) < 17
-                ? api_data.accelLowX
-                : api_data.accelHighX;
+    if (apiData.accelLowX != undefined && apiData.accelHighX != undefined) {
+        processedData.accelX =
+            Math.abs(apiData.accelHighX) < 17
+                ? apiData.accelLowX
+                : apiData.accelHighX;
     }
-    if (api_data.accelLowY != undefined && api_data.accelHighY != undefined) {
-        api_data.accelY =
-            Math.abs(api_data.accelHighY) < 17
-                ? api_data.accelLowY
-                : api_data.accelHighY;
+    if (apiData.accelLowY != undefined && apiData.accelHighY != undefined) {
+        processedData.accelY =
+            Math.abs(apiData.accelHighY) < 17
+                ? apiData.accelLowY
+                : apiData.accelHighY;
     }
-    if (api_data.accelLowZ != undefined && api_data.accelHighZ != undefined) {
-        api_data.accelZ =
-            Math.abs(api_data.accelHighZ) < 17
-                ? api_data.accelLowZ
-                : api_data.accelHighZ;
+    if (apiData.accelLowZ != undefined && apiData.accelHighZ != undefined) {
+        processedData.accelZ =
+            Math.abs(apiData.accelHighZ) < 17
+                ? apiData.accelLowZ
+                : apiData.accelHighZ;
     }
 
     // Altitude
     // Track maximum altitude
-    if (altitudeMax == undefined || api_data.altitude > altitudeMax) {
-        altitudeMax = api_data.altitude;
+    if (altitudeMax == undefined || apiData.altitude > altitudeMax) {
+        altitudeMax = apiData.altitude;
     }
-    api_data.altitudeMax = altitudeMax;
-    return api_data;
+    processedData.altitudeMax = altitudeMax;
+
+    // Return processed data
+    return processedData;
 }
 
 API_socketConnect();
