d6d7bb9909c6affd3f94ee69fc21ca189a715e40
Amber Taylor <167683926+s4105951@users.noreply.github.com>
2025-08-11 14:18:52 +1000

    Added module configuration

--- frontend/server.py
+++ frontend/server.py
@@ -27,6 +27,7 @@ def create_app():
         '.png','.jpg','.ico','.svg',    # Images
     )
 
+
     """
     # Custom logging
     if logger != None:
@@ -40,10 +41,51 @@ def create_app():
         app.logger.setLevel(logging.DEBUG)
     """
 
-    # Serve main layout
+
+    # Render modular layout
     @app.route('/')
     def index():
+        # Parse app.config and pre-process modular layout information for CSS
+        # Generate CSS selectors for pages
+        app.config["CSS"] = ", ".join(["#{0} .{0}".format(page["id"]) for page in app.config["PAGES"]]) + " {display: flex;}";
+    
+        # Generate positional classes for modules
+        grid = set()
+        for module in app.config["MODULES"]:
+            # All modules are hidden by default
+            class_list = {"module", "hidden"}
+
+            # For each module, update visibility and position for each page
+            for page in app.config["MODULES"][module]["pages"]:
+                # Encode position and size in grid
+                cols = "{}-c-{}-{}".format(page[0], page[1], page[3])
+                rows = "{}-r-{}-{}".format(page[0], page[2], page[4])
+                
+                # Add classes to grid
+                grid.add("#{} .{}".format(page[0], cols))
+                grid.add("#{} .{}".format(page[0], rows))
+
+                # Add classes to module
+                class_list.add(page[0])
+                class_list.add(cols)
+                class_list.add(rows)
+
+            # Assign generated classes to module
+            app.config["MODULES"][module]["classes"] = ' '.join(class_list)
+
+        # Add optimised grid to CSS
+        for grid_class in grid:
+            grid_type, grid_start, grid_span = grid_class.split("-")[-3:]
+            app.config["CSS"] += "\n{} {{grid-{}: {} / span {};}} ".format(
+                grid_class,
+                "column" if grid_type == "c" else "row",
+                int(grid_start)+1,
+                grid_span
+            )
+        
+        # Render the page
         return render_template("layout.html", config=app.config)
+
 
     # Serve static files and HTML pages
     @app.route('/<path:filename>')
@@ -66,11 +108,11 @@ def create_app():
             #app.logger.warning(f"404 not found: {filename}")
             abort(404)
 
+
     # Debugging
     @app.route('/debug/api')
     def debug_api():
         return render_template("debug_api.html")
-    
     @app.route('/debug/modules')
     def debug_modules():
         return render_template("debug_modules.html")
