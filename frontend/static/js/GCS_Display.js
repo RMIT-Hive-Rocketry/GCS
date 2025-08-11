c2c88bb5f5d089e6f0e390c58167cda969256917
Amber Taylor <167683926+s4105951@users.noreply.github.com>
2025-08-11 14:18:52 +1000

    Added module configuration

--- frontend/static/js/GCS_Display.js
+++ frontend/static/js/GCS_Display.js
@@ -1,64 +1,10 @@
 /**
  * GCS Display
  *
- * Responsible for switching tabs/pages, button logic, etc.
- * Updates stats on the webpage based on the API
+ * Responsible for updating the webpage based on the API
  *
  * Functions and constants should be prefixed with "display"
  */
-
-// DYNAMIC MODULE SWITCHING CODE
-function displaySelectModule(selected) {
-    document.querySelectorAll(".module").forEach((elem) => {
-        if (elem.classList.contains(selected)) {
-            elem.classList.remove("hidden");
-        } else {
-            elem.classList.add("hidden");
-        }
-    });
-}
-
-const selectedClasses = ["selected"];
-
-if (document.querySelector("nav")) {
-    // Get selected page
-    var selected = window.location.hash
-        ? this.window.location.hash.substring(1)
-        : document.querySelector("nav a").href.split("#")[1];
-
-    // Determine which modules are "selected"
-    displaySelectModule(selected);
-
-    // Highlight selected tab link
-    document
-        .querySelector(`a[href='#${selected}']`)
-        .classList.add(...selectedClasses);
-
-    // Switch tabs when links are pressed
-    document.querySelectorAll("nav a").forEach((elem) => {
-        elem.addEventListener("click", (event) => {
-            event.preventDefault();
-
-            // Switch tabs
-            selected = elem.href.split("#")[1];
-            displaySelectModule(selected);
-
-            // Highlight new selected tab link
-            document.querySelectorAll("nav a").forEach((elem) => {
-                elem.classList.remove(...selectedClasses);
-            });
-            document
-                .querySelector(`a[href='#${selected}']`)
-                .classList.add(...selectedClasses);
-
-            // Update URL
-            history.replaceState(null, "", `#${selected}`);
-
-            // Dispatch page resize event (since elements are moving around)
-            window.dispatchEvent(new Event("resize"));
-        });
-    });
-}
 
 
 // FUNCTIONS FOR UPDATING DISPLAY ITEMS
