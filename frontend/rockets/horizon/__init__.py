bf1e56afbf34debcc3f2d7c79f1ae12b57286f6d
Amber Taylor <167683926+s4105951@users.noreply.github.com>
2026-02-22 16:51:36 +1100

    Begin basic Horizon interface

--- frontend/rockets/horizon/__init__.py
+++ frontend/rockets/horizon/__init__.py
@@ -12,16 +12,40 @@ class ConfigHorizon(Config):
 
         self.ROCKET_NAME = "Horizon"
         self.LOGO = "horizon/static/img/logo-horizon-gradient.png"
-        self.STYLESHEETS = []
+        self.STYLESHEETS = ["horizon/static/css/horizon.css"]
         self.GRID = (24, 12)
 
         # Load Horizon modules
-        self.MODULES = ["horizon/modules/horizon_logos.html"]
+        self.MODULES = [
+            "horizon/modules/horizon_logos.html",
+            "horizon/modules/horizon_nav.html",
+            "horizon/modules/horizon_overview.html",
+            "horizon/modules/horizon_radio.html",
+        ]
 
         # Define pages for Horizon
         self.PAGES = [
-            {"name": "Main Interface", "icon": "icon-rocket", "id": "page-main"},
+            {"name": "Overview", "icon": "icon-rocket", "id": "page-main"},
+            {"name": "Pre-flight", "icon": "icon-tasks", "id": "page-preflight"},
+            {"name": "Control", "icon": "icon-gamepad", "id": "page-control"},
         ]
 
         # Module positioning on each page
-        self.MODULE_PAGES = {}
+        self.MODULE_PAGES = {
+            "horizon_logos": [
+                ("page-main", 0, 0, 12, 1),
+                ("page-preflight", 0, 0, 12, 1),
+                ("page-control", 0, 0, 12, 1),
+            ],
+            "horizon_radio": [
+                ("page-main", 12, 0, 12, 1),
+                ("page-preflight", 12, 0, 12, 1),
+                ("page-control", 12, 0, 12, 1),
+            ],
+            "horizon_nav": [
+                ("page-main", 0, 11, 24, 1),
+                ("page-preflight", 0, 11, 24, 1),
+                ("page-control", 0, 11, 24, 1),
+            ],
+            "horizon_overview": [("page-main", 0, 1, 24, 10)],
+        }
