e803f8ffc982677cd1d6000ab967fe5562af2688
XavierEgan <xavieregan2@gmail.com>
2026-03-01 20:23:44 +1100

    fix seg fault on mac

--- backend/tools/debug_pygame_controller.py
+++ backend/tools/debug_pygame_controller.py
@@ -2,9 +2,11 @@ import pygame
 import time
 
 pygame.init()
+pygame.joystick.init()
 
 while pygame.joystick.get_count() == 0:
     print("No controllers detected.")
+    pygame.event.pump()
     time.sleep(0.5)
 
 joystick = pygame.joystick.Joystick(0)
@@ -14,6 +16,7 @@ print(f"Controller detected: {joystick.get_name()}")
 
 running = True
 while running:
+    pygame.event.pump()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
