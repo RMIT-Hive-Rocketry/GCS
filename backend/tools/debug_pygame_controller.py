91c1196ab33d5c9709d2ac03b2dac7fb766a186d
Amber Taylor <167683926+s4105951@users.noreply.github.com>
2026-03-08 16:48:10 +1100

    add bindings to debug_pygame_controller.py

--- backend/tools/debug_pygame_controller.py
+++ backend/tools/debug_pygame_controller.py
@@ -15,24 +15,50 @@ joystick.init()
 
 print(f"Controller detected: {joystick.get_name()}")
 
+PENDANT_BINDINGS = {
+    1: "IGNITION_O2",
+    2: "IGNITION_FIRE",
+    3: "FILL_PURGE",
+    4: "IGNITION",
+    5: "ESTOP",
+    6: "FILL",
+    8: "FILL_N2O"
+}
+
+PENDANT_ACTIVE = set()  # Set to keep track of active buttons
+
 running = True
 while running:
     pygame.event.pump()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.JOYAXISMOTION:
+        elif event.type == pygame.JOYAXISMOTION and abs(event.value) > 0.2:
+            # Joystick deadzone to prevent console spam
             #print(f"Joystick Axis: {event.axis}, Value: {event.value}")
-            pass
         elif event.type == pygame.JOYBALLMOTION:
             #print(f"Joystick Ball: {event.ball}, Value: {event.rel}")
             pass
         elif event.type == pygame.JOYHATMOTION:
             print(f"Joystick Hat: {event.hat}, Value: {event.value}")
         elif event.type == pygame.JOYBUTTONDOWN:
-            print(f"Joystick Button {event.button} Down")
+            # Check
+            if event.button in PENDANT_BINDINGS:
+                #print(PENDANT_BINDINGS[event.button])
+                PENDANT_ACTIVE.add(PENDANT_BINDINGS[event.button])
+                print(PENDANT_ACTIVE)
+            else:
+                print(f"Joystick Button {event.button} Down")
         elif event.type == pygame.JOYBUTTONUP:
-            print(f"Joystick Button {event.button} Up")
+            if event.button in PENDANT_BINDINGS:
+                #print(PENDANT_BINDINGS[event.button])
+                PENDANT_ACTIVE.remove(PENDANT_BINDINGS[event.button])
+                if len(PENDANT_ACTIVE) > 0:
+                    print(PENDANT_ACTIVE)
+                else:
+                    print("{}")
+            else:
+                print(f"Joystick Button {event.button} Up")
         elif event.type == pygame.JOYDEVICEADDED:
             print("New controller connected.")
         elif event.type == pygame.JOYDEVICEREMOVED:
