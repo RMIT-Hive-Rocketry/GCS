22bdf6ca62542733d5748e7b3d9671e6c0c6e863
White_Noisex <joseph4d2007@gmail.com>
2026-03-22 17:05:52 +1100

    update service

--- backend/dummy_alert.py
+++ backend/dummy_alert.py
@@ -3,12 +3,6 @@ import time
 
 def main():
 
-    slogger.debug("Template Service Debug Test Message")
-    slogger.info("Template Service info Test Message")
-    slogger.success("Template Service success Test Message")
-    slogger.warning("Template Service warning Test Message")
-    slogger.error("Template Service  error Test Message")
-    slogger.critical("Template Service critical Test Message")
     while True:
         slogger.critical("hello")
         time.sleep(1)
