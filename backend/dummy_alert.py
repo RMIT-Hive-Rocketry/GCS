04dfd3811834b5707146d07d145e791645a8caf4
White_Noisex <joseph4d2007@gmail.com>
2026-03-22 17:47:24 +1100

    Added Changes According to Pull

--- backend/dummy_alert.py
+++ backend/dummy_alert.py
@@ -1,9 +1,10 @@
 import backend.includes_python.process_logging as slogger
+import backend.includes_python.service_helper as service_helper
 import time
 
 def main():
 
-    while True:
+    while not service_helper.time_to_stop():
         slogger.critical("hello")
         time.sleep(1)
 
