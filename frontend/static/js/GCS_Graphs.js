9c76e3a8d3dee516ae90061a917d6fe1dea208d9
Iurovet <s4003879@student.rmit.edu.au>
2026-05-12 18:59:45 +1000

    Remove a line that does nothing

--- frontend/static/js/GCS_Graphs.js
+++ frontend/static/js/GCS_Graphs.js
@@ -286,9 +286,6 @@ function graphRender(chart) {
             if (chart !== GRAPH_TEST_COLOURS) {
                 chart.x.domain([windowStart, now]);
             }
-            else {
-                chart.x.domain = chart.x.domain;
-            }
             
             chart.y.domain([
                 Math.min(
