1fddae2a8bd957d13dcffc9c8eaa897e8e30b97c
mov360 <146847389+mov360@users.noreply.github.com>
2026-05-27 00:57:19 +1000

    Embedding notification sound feature in the diagnostic page

--- frontend/static/js/GCS_API.js
+++ frontend/static/js/GCS_API.js
@@ -601,6 +601,10 @@ function API_OnMessage(event) {
             if (typeof graphUpdateDiagnostics === "function") {
                 graphUpdateDiagnostics(apiData);
             }
+
+            if (typeof horizonDiagSoundProcessPacket === "function") {
+                horizonDiagSoundProcessPacket(apiData);
+            }
         }
     } catch (error) {
         console.error("Data processing error:", error);
