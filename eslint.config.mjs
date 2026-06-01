120e0ed509b550098c0b5316f01c96a8e77cf1fb
Amber Taylor <167683926+s4105951@users.noreply.github.com>
2026-06-01 12:05:51 +1000

    add eslint and prettier to precommit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,16 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import antfu from '@antfu/eslint-config'
+import css from '@eslint/css'
+import js from '@eslint/js'
+import json from '@eslint/json'
+import markdown from '@eslint/markdown'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import globals from 'globals'
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default antfu([
+  { ignores: ['node_modules'] },
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+  { files: ['**/*.md'], plugins: { markdown }, language: 'markdown/gfm', extends: ['markdown/recommended'] },
+  { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
+  eslintConfigPrettier,
+])
