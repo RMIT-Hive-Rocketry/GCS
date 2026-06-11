ccd4ed574d1fd764c4dfe6f3b039a73dd114f892
Amber Taylor <167683926+s4105951@users.noreply.github.com>
2026-06-11 17:05:19 -0500

    remove references to other repo

--- scripts/cleanup_artifacts.sh
+++ scripts/cleanup_artifacts.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-REPO="RMIT-Hive-Rocketry/GCS-2026"
+REPO="RMIT-Hive-Rocketry/GCS"
 
 for id in $(gh api "repos/$REPO/actions/artifacts" --paginate | jq '.artifacts[].id'); do
   gh api -X DELETE "repos/$REPO/actions/artifacts/$id" >/dev/null
